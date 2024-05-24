ecc2f00adccd246791ecb831f974ecd07bc64f58
chang-ha <kym637@naver.com>
2024-05-24 13:08:42 +0900

    우창하 : 코딩테스트

---
+++ CodeTest/AlternatingCharacters/AlternatingCharacters.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string&);
+string rtrim(const string&);
+
+/*
+ * Complete the 'alternatingCharacters' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int alternatingCharacters(string s) 
+{
+    char PrevChar = 0;
+    int DeleteCount = 0;
+
+    for (char _CurChar : s)
+    {
+        if (_CurChar == PrevChar)
+        {
+            ++DeleteCount;
+        }
+
+        PrevChar = _CurChar;
+    }
+
+    return DeleteCount;
+}
+
+int main()
+{
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = alternatingCharacters(s);
+
+        cout << result << "\n";
+    }
+
+    return 0;
+}
+
+string ltrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
