ffcc7b7f2cf23c656b43bb084d48eaf6a1516899
chang-ha <kym637@naver.com>
2024-05-24 14:03:09 +0900

    우창하 : 코딩테스트

---
+++ CodeTest/181951/181951.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+class MyClass
+{
+public:
+    MyClass()
+        : A(0), B(0)
+    {
+
+    }
+
+    MyClass(int _X, int _Y)
+        : A(_X), B(_Y)
+    {
+
+    }
+
+    ~MyClass() = default;
+
+    friend std::istream& operator>>(std::istream& _Input, MyClass& _MyClass)
+    {
+        _Input >> _MyClass.A >> _MyClass.B;
+        return _Input;
+    }
+
+    friend std::ostream& operator<<(std::ostream& _Output, const MyClass& _MyClass)
+    {
+        _Output << "a = " << _MyClass.A << std::endl << "b = " << _MyClass.B << std::endl;
+        return _Output;
+    }
+
+private:
+    int A = 0;
+    int B = 0;
+};
+
+int main(void) 
+{
+    // int a;
+    // int b;
+    // cin >> a >> b;
+    // cout << "a = " << a << endl;
+    // cout << "b = " << b << endl;
+
+    MyClass InputValue;
+    cin >> InputValue;
+    cout << InputValue;
+
+    return 0;
+}
