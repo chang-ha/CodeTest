be84540ac37ff370c1e6d15d72cd005cc52b65ca
chang-ha <kym637@naver.com>
2024-05-31 14:35:27 +0900

    우창하 : 코딩테스트

---
+++ CodeTest/CommonChild/CommonChild.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'commonChild' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s1
+ *  2. STRING s2
+ */
+
+int commonChild(string s1, string s2) 
+{
+
+}
+
+int main()
+{
+    string s1;
+    getline(cin, s1);
+
+    string s2;
+    getline(cin, s2);
+
+    int result = commonChild(s1, s2);
+
+    cout << result << "\n";
+
+    return 0;
+}
--- CodeTest/Note/Note.cpp
+++ CodeTest/Note/Note.cpp
@@ -1,6 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+class CTest
+{
+public:
+	int  mA;
+
+public:
+	CTest Create()
+	{
+		CTest mCTest;
+		return mCTest;
+	}
+};
+
+
+class CSingleton
+{
+private:
+	static CSingleton* mCSingleton;
+	CSingleton() = default;
+
+	CSingleton(const CSingleton& _Other) = delete;
+	CSingleton(CSingleton&& _Other) noexcept = delete;
+	CSingleton& operator=(const CSingleton& _Other) = delete;
+	CSingleton& operator=(CSingleton&& _Other) noexcept = delete;
+
+public:
+	static CSingleton* GetSingleton()
+	{
+		if (nullptr == mCSingleton)
+		{
+			mCSingleton = new CSingleton();
+		}
+
+		return mCSingleton;
+	}
+};
+
+CSingleton* CSingleton::mCSingleton = nullptr;
+
 int main()
 {
+	CTest* a = new CTest();
+	CTest* b = new CTest();
+	CTest (CTest::*address)() = &CTest::Create;
+	CSingleton* s = CSingleton::GetSingleton();
+	int x = 0;
 }
