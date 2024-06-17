da40655d96191045de84a2c697f501025311759e
chang-ha <kym637@naver.com>
2024-06-17 14:08:00 +0900

    우창하 : 코딩테스트

--- CodeTest/Note/Note.cpp
+++ CodeTest/Note/Note.cpp
@@ -1,7 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int luckBalance(int k, vector<vector<int>> contests)
+{
+	int Result = 0;
+	std::multiset<int, std::greater<int>> LuckContainer;
+
+	for (vector<int> CurLuck : contests)
+	{
+		if (2 != CurLuck.size())
+		{
+			// Error
+			return INT_MIN;
+		}
+
+		if (0 == CurLuck[1])
+		{
+			Result += CurLuck[0];
+		}
+		else
+		{
+			LuckContainer.insert(CurLuck[0]);
+		}
+	}
+
+	for (int CurLuck : LuckContainer)
+	{
+		if (0 < k)
+		{
+			--k;
+			Result += CurLuck;
+		}
+		else
+		{
+			Result -= CurLuck;
+		}
+	}
+
+	return Result;
+}
 
 int main()
 {
+	luckBalance(3, { {5,1}, {2,1}, {1,1}, {8,1}, {10,0}, {5,0} });
 }
