c79b4f6630e74615660c7ffc6807cb6a7a1c8cf7
chang-ha <kym637@naver.com>
2024-06-19 10:03:47 +0900

    우창하 : 코딩테스트

--- CodeTest/MaxMin/MaxMin.cpp
+++ CodeTest/MaxMin/MaxMin.cpp
@@ -14,9 +14,31 @@ string rtrim(const string&);
  *  2. INTEGER_ARRAY arr
  */
 
-int maxMin(int k, vector<int> arr) 
+int maxMin(int k, vector<int> arr)
 {
+    int Result = INT_MAX;
 
+    size_t Arr_Size = arr.size();
+    if (Arr_Size < k || 1 >= k)
+    {
+        // Error
+        return Result;
+    }
+
+    std::sort(arr.begin(), arr.end());
+
+    if (Arr_Size == k)
+    {
+        Result = arr[Arr_Size - 1] - arr[0];
+        return Result;
+    }
+
+    size_t LoopCount = Arr_Size - static_cast<size_t>(k) + 1;
+    for (size_t i = 0; i < LoopCount; i++)
+    {
+        Result = std::min(Result, arr[i + k - 1] - arr[i]);
+    }
+    return Result;
 }
 
 int main()
--- CodeTest/Note/Note.cpp
+++ CodeTest/Note/Note.cpp
@@ -1,37 +1,4 @@
-using namespace std;
-
-#include <set>
-#include <vector>
-#include <algorithm>
-
-int maxMin(int k, vector<int> arr)
-{
-	int Result = INT_MAX;
-
-	size_t Arr_Size = arr.size();
-	if (Arr_Size < k || 1 >= k)
-	{
-		// Error
-		return Result;
-	}
-
-	std::sort(arr.begin(), arr.end());
-
-	if (Arr_Size == k)
-	{
-		Result = arr[Arr_Size - 1] - arr[0];
-		return Result;
-	}
-
-	size_t LoopCount = Arr_Size - static_cast<size_t>(k) + 1;
-	for (size_t i = 0; i < LoopCount; i++)
-	{
-		Result = std::min(Result, arr[i + k - 1] - arr[i]);
-	}
-	return Result;
-}
 
 int main()
 {
-	maxMin(3, { 10,100,300,200,1000,20,30 });
 }
