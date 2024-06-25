c25fcac8a01e1440475c7ded236047be7f8d23b5
chang-ha <kym637@naver.com>
2024-06-25 09:47:45 +0900

    우창하 : 코딩테스트

--- CodeTest/Note/Note.cpp
+++ CodeTest/Note/Note.cpp
@@ -1,5 +1,15 @@
+#include <string>
+#include <vector>
 
+using namespace std;
+
+int solution(int num, int n)
+{
+    int answer = (num % n == 0) ? 1 : 0;
+    return answer;
+}
 
 int main()
 {
+    solution(34,3);
 }
