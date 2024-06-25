c13c2695e512ea877b97727bbaed30880fbb7f42
chang-ha <kym637@naver.com>
2024-06-25 09:50:09 +0900

    우창하 : 코딩테스트

--- CodeTest/181937/181937.cpp
+++ CodeTest/181937/181937.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-int solution(int num, int n) 
+int solution(int num, int n)
 {
-    int answer = 0;
+    int answer = (num % n == 0) ? 1 : 0;
     return answer;
 }
--- CodeTest/Note/Note.cpp
+++ CodeTest/Note/Note.cpp
@@ -1,15 +1,5 @@
-#include <string>
-#include <vector>
 
-using namespace std;
-
-int solution(int num, int n)
-{
-    int answer = (num % n == 0) ? 1 : 0;
-    return answer;
-}
 
 int main()
 {
-    solution(34,3);
 }
