add37d2761bb6de095f64eb7f979b95813affb64
chang-ha <kym637@naver.com>
2024-06-28 17:27:13 +0900

    우창하 : 코딩테스트 주석 추가

--- CodeTest/131128/131128.cpp
+++ CodeTest/131128/131128.cpp
@@ -7,6 +7,7 @@ string solution(string X, string Y)
 {
 	string answer = "";
 
+	// Vector에 문자열 갯수 저장
 	std::vector<int> X_Vector(10);
 	for (const char _CurChar : X)
 	{
@@ -19,16 +20,21 @@ string solution(string X, string Y)
 		Y_Vector[_CurChar - '0'] += 1;
 	}
 
+	// 가장 큰 수를 구해야 하니 9부터 for loop
+	// 두 벡터중 숫자가 적은 값 == 공통 갯수
+	// 공통 갯수만큼 answer 문자열에 추가
 	for (int i = 9; i >= 0; i--)
 	{
 		answer.append(std::min(X_Vector[i], Y_Vector[i]), '0' + i);
 	}
 
+	// 공통된 수가 없을 때
 	if ("" == answer)
 	{
 		answer = "-1";
 	}
-
+	
+	// 공통된 수가 0뿐일 때
 	if (answer.begin() != answer.end() &&
 		'0' == *answer.begin())
 	{
