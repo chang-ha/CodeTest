42bd6e9c01c6c2694f51dc8ef5025868a7f88c4e
chang-ha <kym637@naver.com>
2024-07-10 11:50:12 +0900

    우창하 : 코딩테스트

---
+++ CodeTest/181188/181188.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include <algorithm>
+
+bool greaterVector(const std::vector<int>& _Vector1, const std::vector<int>& _Vector2)
+{
+	return _Vector1[1] < _Vector2[1];
+}
+
+int solution(vector<vector<int>> targets)
+{
+	int answer = 0;
+
+	std::sort(targets.begin(), targets.end(), greaterVector);
+
+	int LaunchPos = -1;
+	for (const vector<int>& _CurTarget : targets)
+	{
+		if (_CurTarget[0] >= LaunchPos)
+		{
+			++answer;
+			LaunchPos = _CurTarget[1];
+		}
+	}
+
+	return answer;
+}
