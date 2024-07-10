5586a48e62caa7e400309437e900e4796b86d095
chang-ha <kym637@naver.com>
2024-07-10 11:59:16 +0900

    우창하 : 코딩테스트

--- CodeTest/Note/Note.cpp
+++ CodeTest/Note/Note.cpp
@@ -1,5 +1,15 @@
 #include <TimeCheck\TimeCheck.h>
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2)
+{
+	return num1 * num2;
+}
+
 int main()
 {
 	TIME_UNIT ATime = CheckFunctionTime([&]()
