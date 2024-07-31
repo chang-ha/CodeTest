9c91aa6e7c9ab7f7c5328b4831491344e397ea12
chang-ha <kym637@naver.com>
2024-07-31 15:25:13 +0900

    우창하 : 코딩테스트

---
+++ CodeTest/181906/181906.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include <set>
+int solution(string my_string, string is_suffix)
+{
+    std::set<string> Subset;
+
+    size_t String_Size = my_string.size();
+    for (size_t i = 0; i < String_Size; i++)
+    {
+        Subset.insert(my_string.substr(0, i));
+    }
+
+    if (Subset.end() == Subset.find(is_suffix))
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
