f986572c7ec14de1f3a09ac88253637dc9de86a2
chang-ha <kym637@naver.com>
2024-08-01 13:43:27 +0900

    우창하 : 코딩테스트

---
+++ CodeTest/82612/82612.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = money - static_cast<long long>(price) * count * (count + 1) / 2;
+
+    if (answer > 0)
+    {
+        answer = 0;
+    }
+    else
+    {
+        answer *= -1;
+    }
+
+    return answer;
+}
