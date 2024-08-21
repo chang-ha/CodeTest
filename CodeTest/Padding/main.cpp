394a1027a7289fe454d8379033ab5efbf770eacc
chang-ha <kym637@naver.com>
2024-08-21 14:54:23 +0900

    우창하 : 바이트 패딩 및 alignas

--- CodeTest/Padding/main.cpp
+++ CodeTest/Padding/main.cpp
@@ -28,6 +28,72 @@ struct PaddedData {
 	std::vector<char> padding;
 };
 
+class BytePaddingConsider
+{
+public:
+	BytePaddingConsider() = default;
+	~BytePaddingConsider() = default;
+
+private:
+	char C = '1'; // 1바이트
+	short E = 1; // 2바이트
+	int A = 1; // 4바이트
+	// BytePadding으로 인해 8바이트 차지
+
+	long long B = 1; // 8바이트
+	double D = 1.0; // 8바이트
+	long long F = 1;// 8바이트
+	// 총 32바이트 차지
+};
+
+class BytePaddingNotConsider
+{
+public:
+	BytePaddingNotConsider() = default;
+	~BytePaddingNotConsider() = default;
+
+private:
+	int A = 1; // 8바이트 차지
+	long long B = 1; // 8바이트 차지
+	char C = '1'; // 8바이트 차지
+	double D = 1.0; // 8바이트 차지
+	short E = 1; // 8바이트 차지
+	long long F = 1; // 8바이트 차지
+	// 총 48바이트
+};
+
+//class alignas(8) AdjustBytePadding
+//{
+//	char a;  // 8바이트 경계로 정렬
+//	char c;
+//	int b;
+//	// 8바이트
+//};
+
+class AdjustBytePadding
+{
+	alignas(8) char a;  // 8바이트 경계로 정렬
+	char c;
+	int b;
+	// 8바이트
+};
+
+class NotAdjustBytePadding
+{
+	char a;  // Byte가 가장 큰 자료형인 int(4바이트) 기준으로 자동 정렬
+	int b;
+	char c;
+	// 12바이트
+};
+
+class CantAdjustBytePadding
+{
+	alignas(4) char a;  // 4바이트 경계로 정렬 시도
+	char c;
+	long long b; // 8바이트 자료형이 존재
+	// 16바이트
+};
+
 int main()
 {
 	int cpuInfo[4] = { -1 };
@@ -38,7 +104,17 @@ int main()
 
 	unsigned int LoopCount = cacheLineSize - PaddingSize;
 	std::vector<int> A;
-	sizeof(A);
+	// 내부에 1. 포인터, 2. size_t 배열 size, 3. size_t 배열 capacity
+	// 32bit -> 16byte (12byte에서 bytepadding으로 인해 16byte)
+	// 64bit -> 32byte (24byte에서 bytepadding으로 인해 32byte)
+ 	size_t A_Size = sizeof(A);
+
+	size_t ConsiderBytePadding = sizeof(BytePaddingConsider);
+	size_t NotConsiderBytePadding = sizeof(BytePaddingNotConsider);
+
+	size_t Adjust = sizeof(AdjustBytePadding);
+	size_t NotAdjust = sizeof(NotAdjustBytePadding);
+	size_t CantAdjust = sizeof(CantAdjustBytePadding);
 
 	UnalignedData Unalignedarray[10] = {};
 	PaddedData Paddedarray[10] = {};
