c0b7ce5483ecb4ba4e0f294ae0ed0c0be09d7671
chang-ha <kym637@naver.com>
2024-08-22 14:57:48 +0900

    우창하 : 지역 변수의 패딩 효율성

--- CodeTest/MemorySection/main.cpp
+++ CodeTest/MemorySection/main.cpp
@@ -5,6 +5,8 @@ void Function()
 
 }
 
+void Function2();
+
 static int sA = 100;
 static int sB;
 static long long TestArray1[1024 * 1024 * 100];
@@ -16,26 +18,37 @@ int main()
 	int* nA = new int;
 	int* nB = new int;
 
+	char A = '1';
 	short B = 1;
 	short C = 1;
 	short D = 1;
 	short E = 1;
 	short F = 1;
 
-	std::cout << "Code Memory : " << &main << std::endl;
-	std::cout << "Code Memory : " << &Function << std::endl;
+	// 함수의 주소는 선언부에 따라 다름
+	std::cout << "Code Memory(main) : " << reinterpret_cast<long long>(&main) << std::endl;
+	std::cout << "Code Memory(function) : " << reinterpret_cast<long long>(&Function )<< std::endl;
+	std::cout << "Code Memory(function2) : " << reinterpret_cast<long long>(&Function2) << std::endl;
 
 
-	std::cout << "Init Data Memory : " << &sA << ", Init Data Value : " << sA << std::endl;
-	std::cout << "Not Init Data Memory : " << &sB << ", Init Data Value : " << sB << std::endl;
+	std::cout << "Init Data Memory : " << reinterpret_cast<long long>(&sA) << ", Init Data Value : " << sA << std::endl;
+	std::cout << "Not Init Data Memory : " << reinterpret_cast<long long>(&sB) << ", Init Data Value : " << sB << std::endl;
+	std::cout << "Not Init Array Memory : " << reinterpret_cast<long long>(&TestArray1) << std::endl;
 
-	std::cout << "Heap Memory : " << nA << std::endl;
-	std::cout << "Heap Memory : " << nB << std::endl;
+	std::cout << "Heap Memory : " << reinterpret_cast<long long>(nA) << std::endl;
+	std::cout << "Heap Memory : " << reinterpret_cast<long long>(nB) << std::endl;
 
-	std::cout << "Stack Memory : " << &B << std::endl;
-	std::cout << "Stack Memory : " << &C << std::endl;
-	std::cout << "Stack Memory : " << &D << std::endl;
-	std::cout << "Stack Memory : " << &E << std::endl;
-	std::cout << "Stack Memory : " << &F << std::endl;
+	// 내 컴퓨터 환경에서는 Stack의 주소값이 가장 작음
+	std::cout << "Stack Memory : " << reinterpret_cast<long long>(&A) << std::endl;
+	std::cout << "Stack Memory : " << reinterpret_cast<long long>(&B) << std::endl;
+	std::cout << "Stack Memory : " << reinterpret_cast<long long>(&C) << std::endl;
+	std::cout << "Stack Memory : " << reinterpret_cast<long long>(&D) << std::endl;
+	std::cout << "Stack Memory : " << reinterpret_cast<long long>(&E) << std::endl;
+	std::cout << "Stack Memory : " << reinterpret_cast<long long>(&F) << std::endl;
+
+}
+
+void Function2()
+{
 
 }
--- CodeTest/Thread&StackOverflow/main.cpp
+++ CodeTest/Thread&StackOverflow/main.cpp
@@ -6,6 +6,7 @@ static int LoopCount2 = 0;
 static int LoopCount3 = 0;
 static int LoopCount4 = 0;
 static int LoopCount5 = 0;
+
 void Recursive1()
 {
 	++LoopCount1;
@@ -27,26 +28,110 @@ void Recursive4()
 	++LoopCount4;
 	Recursive4();
 }
+
 void Recursive5()
 {
 	++LoopCount5;
 	Recursive5();
 }
-void InfinityLoop()
+
+void LocalMember()
 {
-	Recursive1();
+	// 2600대
+	++LoopCount1;
+	long long A = 1; // 8바이트 할당 + 24바이트 패딩
+	long long B = 1; // 8바이트 할당 + 24바이트 패딩
+	long long C = 1; // 8바이트 할당 + 24바이트 패딩
+	long long D = 1; // 8바이트 할당 + 24바이트 패딩
+	// long long A_ptr = reinterpret_cast<long long>(&A);
+	// long long B_ptr = reinterpret_cast<long long>(&B);
+	// long long C_ptr = reinterpret_cast<long long>(&C);
+	// long long D_ptr = reinterpret_cast<long long>(&D);
+
+	LocalMember();
 }
 
+struct MyStruct_32Byte
+{
+	long long A = 1;
+	long long B = 2;
+	long long C = 3;
+	long long D = 4;
+};
+
+struct MyStruct_28Byte
+{
+	long long A = 1;
+	long long B = 2;
+	long long C = 3;
+	int D = 4;
+};
+
+struct MyStruct_24Byte
+{
+	long long A = 1;
+	long long B = 2;
+	long long C = 3;
+};
+
+struct MyStruct_16Byte
+{
+	long long A = 1;
+	long long B = 2;
+};
+
+struct MyStruct_1Byte
+{
+	char A = '1';
+};
+
+struct MyStruct_48Byte
+{
+	long long A = 1;
+	long long B = 2;
+	long long C = 3;
+	long long D = 4;
+	long long E = 5;
+	long long F = 6;
+};
+
+
+void LocalStruct()
+{
+	// 3200대
+	++LoopCount2;
+	MyStruct_32Byte mMystruct; // 32byte 할당 + 32byte 패딩 (3200대)
+	// MyStruct_28Byte mMystruct2;// 28byte 할당 + 36byte 패딩
+	// MyStruct_24Byte mMystruct3;// 24byte 할당 + 24byte 패딩
+	// MyStruct_16Byte mMystruct4;// 16byte 할당 + 32byte 패딩
+	// MyStruct_1Byte mMystruct5;// 1byte 할당 + 35byte 패딩 (3500대)
+	// MyStruct_48Byte mMystruct6;// 48byte 할당 + 32byte 패딩 (3500대)
+	// long long Struct_ptr = reinterpret_cast<long long>(&mMystruct);
+	// long long Struct_ptr2 = reinterpret_cast<long long>(&mMystruct2);
+	// long long Struct_ptr3 = reinterpret_cast<long long>(&mMystruct3);
+	// long long Struct_ptr4 = reinterpret_cast<long long>(&mMystruct4);
+	// long long Struct_ptr5 = reinterpret_cast<long long>(&mMystruct5);
+	// long long Struct_ptr6 = reinterpret_cast<long long>(&mMystruct6);
+	LocalStruct();
+}
 
 int main()
 {
-	std::thread A1(Recursive1);
-	std::thread A2(Recursive2);
-	std::thread A3(Recursive3);
-	std::thread A4(Recursive4);
-	Recursive5();
-	A1.join();
-	A2.join();
-	A3.join();
-	A4.join();
+	// std::thread A1(Recursive1);
+	// std::thread A2(Recursive2);
+	// std::thread A3(Recursive3);
+	// std::thread A4(Recursive4);
+	// Recursive5();
+	// A1.join();
+	// A2.join();
+	// A3.join();
+	// A4.join();
+
+	// 동일하게 32byte 변수를 사용했지만
+	// 8바이트 지역 변수 사용시 -> 지역변수가 128바이트 스택 메모리 사용
+	// 32바이트 구조체 사용시 -> 지역변수가 64바이트 스택 메모리 사용
+	std::thread StackFrame1(LocalMember);
+	std::thread StackFrame2(LocalStruct);
+	StackFrame1.join();
+	StackFrame2.join();
 }
