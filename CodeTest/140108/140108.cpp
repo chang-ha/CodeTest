622f8aeb86d07e7cc6ea99c43706e7a52890f909
chang-ha <kym637@naver.com>
2024-08-26 15:39:07 +0900

    우창하 : 스터디용 주석 추가

--- CodeTest/140108/140108.cpp
+++ CodeTest/140108/140108.cpp
@@ -13,22 +13,27 @@ int solution(string s)
 
 	for (const char _CurChar : s)
 	{
+		// 첫 번째 글자
 		if (' ' == FirstWord)
 		{
 			FirstWord = _CurChar;
 		}
 
+		// 현재 글자와 첫 글자 같으면
 		if (_CurChar == FirstWord)
 		{
 			++FirstWordCount;
 		}
 		else
 		{
+			// 현재 글자와 첫 글자가 다르면
 			++OtherWordCount;
 		}
 
+		// 글자 수가 같으면
 		if (FirstWordCount == OtherWordCount)
 		{
+			// 초기화
 			++answer;
 			FirstWord = ' ';
 			FirstWordCount = 0;
@@ -36,6 +41,7 @@ int solution(string s)
 		}
 	}
 
+	// 나머지 문자
 	// 문자가 분해되지 않음
 	if (' ' != FirstWord)
 	{
--- CodeTest/176963/176963.cpp
+++ CodeTest/176963/176963.cpp
@@ -8,16 +8,16 @@ vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<st
 {
     vector<int> answer;
 
+    // 이름과 점수 사이즈 차이 존재시 에러
     size_t yearning_size = yearning.size();
-
     if (yearning_size != name.size())
     {
         // Error
         return answer;
     }
 
+    // 이름과 점수 매핑
     std::unordered_map<std::string, int> MemoryScore;
-
     for (size_t i = 0; i < yearning_size; i++)
     {
         MemoryScore[name[i]] = yearning[i];
@@ -29,15 +29,18 @@ vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<st
     {
         int memory = 0;
 
+        // 현재 사진의 점수 계산
         const std::vector<string>& _CurPhoto = photo[i];
         for (const std::string& _CurName : _CurPhoto)
         {
             std::unordered_map<std::string, int>::const_iterator _CurMemoryScore = MemoryScore.find(_CurName);
             if (MemoryScore.end() == _CurMemoryScore)
             {
+                // 이름이 없으면 continue
                 continue;
             }
 
+            // 있으면 점수에 합산
             memory += _CurMemoryScore->second;
         }
 
