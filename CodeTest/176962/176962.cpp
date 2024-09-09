fd72501cc86d46ad52f387ea61506a0cb2b256ed
chang-ha <kym637@naver.com>
2024-09-09 15:31:13 +0900

    우창하 : 코딩테스트

--- CodeTest/176962/176962.cpp
+++ CodeTest/176962/176962.cpp
@@ -19,28 +19,60 @@ int CalcuTime(const std::string& _Time)
 	int Hour = std::stoi(_Time.substr(0, DividePos));
 	int Minute = std::stoi(_Time.substr(DividePos + 1));
 
-	return;
+	return Hour * 60 + Minute;
 }
+
 #include <stack>
-vector<string> solution(vector<vector<string>> plans) 
+#include <tuple>
+#include <algorithm>
+vector<string> solution(vector<vector<string>> plans)
 {
-    std::vector<std::string> answer;
-	std::stack<std::vector<std::string>> RemainPlans;
+	std::sort(plans.begin(), plans.end(), [](const std::vector<string>& _First, const std::vector<string>& _Second)
+		{
+			return _First[1] < _Second[1];
+		});
+
+	std::vector<std::string> answer;
+	std::stack<std::tuple<const std::string, int, int>> RemainPlans;
 
 	for (const std::vector<std::string>& _CurPlan : plans)
 	{
 		if (3 != _CurPlan.size())
 		{
 			// Error
-			return {"계획이 잘못 들어왔습니다."};
+			return { "계획이 잘못 들어왔습니다." };
 		}
 
+		// 스택에 계획이 들어있으면 계산
 		if (false == RemainPlans.empty())
 		{
+			int CurTime = CalcuTime(_CurPlan[1]) - get<1>(RemainPlans.top());
+
+			while (0 < CurTime && false == RemainPlans.empty())
+			{
+				int& PrevPlanTime = get<2>(RemainPlans.top());
+				int SavePrevTime = PrevPlanTime;
+				PrevPlanTime -= CurTime;
+				CurTime -= SavePrevTime;
+
+				if (PrevPlanTime > 0)
+				{
+					continue;
+				}
+
+				answer.push_back(get<0>(RemainPlans.top()));
+				RemainPlans.pop();
+			}
 		}
 
-		RemainPlans.push(_CurPlan);
+		RemainPlans.push(std::make_tuple(_CurPlan[0], CalcuTime(_CurPlan[1]), std::stoi(_CurPlan[2])));
 	}
 
-    return answer;
+	while (false == RemainPlans.empty())
+	{
+		answer.push_back(get<0>(RemainPlans.top()));
+		RemainPlans.pop();
+	}
+
+	return answer;
 }
--- CodeTest/Note/Note.cpp
+++ CodeTest/Note/Note.cpp
@@ -1,56 +1,8 @@
 #include <iostream>
 #include <TimeCheck\TimeCheck.h>
 
-#include <string>
-#include <vector>
-
-using namespace std;
-
-int CalcuTime(const std::string& _Time)
-{
-	size_t DividePos = _Time.find_first_of(":");
-
-	if (std::string::npos == DividePos)
-	{
-		// Error
-		return -1;
-	}
-
-	int Hour = std::stoi(_Time.substr(0, DividePos));
-	int Minute = std::stoi(_Time.substr(DividePos + 1));
-
-	return Hour * 60 + Minute;
-}
-
-#include <stack>
-#include <tuple>
-vector<string> solution(vector<vector<string>> plans)
-{
-	std::vector<std::string> answer;
-	std::stack<std::tuple<const std::string, int, int>> RemainPlans;
-
-	for (const std::vector<std::string>& _CurPlan : plans)
-	{
-		if (3 != _CurPlan.size())
-		{
-			// Error
-			return { "계획이 잘못 들어왔습니다." };
-		}
-
-		if (false == RemainPlans.empty())
-		{
-
-		}
-
-		RemainPlans.push(std::make_tuple(_CurPlan[0], CalcuTime(_CurPlan[1]), std::stoi(_CurPlan[2])));
-	}
-
-	return answer;
-}
-
 int main()
 {
-	solution({ {"korean", "11:40", "30" }, {"english", "12:10", "20"}, { "math", "12:30", "40" }});
 	TIME_UNIT ATime = CheckFunctionTime([&]()
 		{
 		});
