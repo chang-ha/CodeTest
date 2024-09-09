2c70b2e36048ad9d273742db8418991f1b275f22
chang-ha <kym637@naver.com>
2024-09-09 18:04:57 +0900

    우창하 : 코딩테스트

---
+++ CodeTest/1844/1844.cpp
@@ -0,0 +1,14 @@
+#include<vector>
+using namespace std;
+
+int BFSsolution(vector<vector<int> > maps)
+{
+    int answer = 0;
+    return answer;
+}
+
+int DFSsolution(vector<vector<int> > maps)
+{
+    int answer = 0;
+    return answer;
+}
--- CodeTest/Note/Note.cpp
+++ CodeTest/Note/Note.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
 #include <TimeCheck\TimeCheck.h>
 
+#include<vector>
+using namespace std;
+
+// 너비 우선 탐색
+// 같은 너비 우선해서 탐색
+#include <queue>
+int BFSsolution(vector<vector<int> > maps)
+{
+	int answer = 0;
+
+	std::vector<std::vector<bool>> VisitMaps;
+
+	const size_t Col_maps_size = maps.size();
+	VisitMaps.resize(Col_maps_size);
+	for (size_t i = 0; i < Col_maps_size; i++)
+	{
+		const size_t Row_maps_size = maps[i].size();
+		VisitMaps[i].resize(Row_maps_size);
+
+		for (size_t j = 0; j < Row_maps_size; j++)
+		{
+			VisitMaps[i][j] = maps[i][j];
+		}
+	}
+
+	return answer;
+}
+
+// 깊이 우선 탐색
+// 노드의 가장 깊은 노드까지 들어가서 탐색
+int DFSsolution(vector<vector<int> > maps)
+{
+	int answer = 0;
+	return answer;
+}
+
 int main()
 {
+	BFSsolution({ {1, 0, 1, 1, 1},{1, 0, 1, 0, 1},{1, 0, 1, 1, 1},{1, 1, 1, 0, 1},{0, 0, 0, 0, 1}});
 	TIME_UNIT ATime = CheckFunctionTime([&]()
 		{
 		});
