de5575b81df5afc28df911a0f7e3704a6b03fd23
chang-ha <kym637@naver.com>
2024-09-10 17:24:59 +0900

    우창하 : 코딩테스트

---
+++ CodeTest/12924/12924.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+
+// ¹®Á¦
+// https://school.programmers.co.kr/learn/courses/30/lessons/12924
+
+using namespace std;
+
+int solution(int n) 
+{
+    int answer = 1; // n = n
+
+    const int Half_n = n / 2;
+    for (int i = 1; i <= Half_n; i++)
+    {
+        int SumValue = i;
+        for (int j = i + 1; j <= Half_n + 1; j++)
+        {
+            SumValue += j;
+            if (n <= SumValue)
+            {
+                break;
+            }
+        }
+
+        if (n == SumValue)
+        {
+            ++answer;
+        }
+    }
+
+    return answer;
+}
