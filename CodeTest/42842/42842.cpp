a24e170b65b3cb656185a75b7d226aa8b99c288b
chang-ha <kym637@naver.com>
2024-09-11 15:27:50 +0900

    우창하 : 코딩테스트

---
+++ CodeTest/42842/42842.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+// 문제
+// https://school.programmers.co.kr/learn/courses/30/lessons/42842
+
+using namespace std;
+
+// 테두리 = 1줄
+// 전체 가로 X = width_yellow + 2
+// 전체 가로 Y = height_yellow + 2
+// X * Y = brown + yellow
+// (X - 2) * (Y - 2) = yellow
+// 2X + 2Y = brown + 4
+// (X - Y) ^ 2 = (X + Y)^2 - 4XY
+
+#include <cmath>
+vector<int> solution(int brown, int yellow)
+{
+	int SumXY = (brown + 4) / 2;
+	int XY = brown + yellow;
+	int DifXY = static_cast<int>(std::sqrt(SumXY * SumXY - 4 * XY));
+	return { (SumXY + DifXY) / 2, (SumXY - DifXY) / 2 };
+}
