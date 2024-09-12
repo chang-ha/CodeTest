569769e9362296b3b4706d7a0baac11a8072dda2
chang-ha <kym637@naver.com>
2024-09-12 16:01:04 +0900

    우창하 : 코딩테스트

---
+++ CodeTest/12981/12981.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include <unordered_set>
+vector<int> solution(int n, vector<string> words) 
+{
+	const std::string& StartWord = words[0];
+	if (2 > StartWord.size())
+	{
+		return { 1,1 };
+	}
+	char PrevLastWord = StartWord.back();
+
+	std::unordered_set<std::string> WordSet;
+	WordSet.insert(StartWord);
+
+	const size_t words_size = static_cast<int>(words.size());
+	for (int i = 1; i < words_size; i++)
+	{
+		const std::string& CurWord = words[i];
+		if (2 > CurWord.size() || PrevLastWord != CurWord[0] || WordSet.end() != WordSet.find(CurWord))
+		{
+			return {i % n + 1, i / n + 1};
+		}
+
+		WordSet.insert(CurWord);
+		PrevLastWord = CurWord.back();
+	}
+
+	return {0, 0};
+}
