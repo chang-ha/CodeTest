46abbc2878eab87bf8d379a0cef7f2a927e9afb6
chang-ha <kym637@naver.com>
2024-09-27 21:39:12 +0900

    우창하 : 코딩테스트

---
+++ CodeTest/64065/64065.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 1개 원소 , = 0
+// 2개 원소 , = 0 + 구분 콤마 1 + 2개 원소 내부 콤마 1 = 2
+// 3개 원소 , = 0 + 2개 원소 2 + 구분 콤마 1 + 3개 원소 내부 콤마 2 = 0 + 2 + 3
+// 4개 원소 , = 0 + 2 + 3 + 구분 콤마 1 + 4개 원소 내부 콤마 3 = 0 + 2 + 3 + 4 
+
+vector<int> solution(string s) 
+{
+    vector<int> answer;
+    return answer;
+}
--- CodeTest/Note/Note.cpp
+++ CodeTest/Note/Note.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 #include <TimeCheck\TimeCheck.h>
 
+#include <string>
+#include <vector>
+
+// 문제
+// https://school.programmers.co.kr/learn/courses/30/lessons/64065
+
+using namespace std;
+
+// 1개 원소 , = 0
+// 2개 원소 , = 0 + 구분 콤마 1 + 2개 원소 내부 콤마 1 = 2
+// 3개 원소 , = 0 + 2개 원소 2 + 구분 콤마 1 + 3개 원소 내부 콤마 2 = 0 + 2 + 3
+// 4개 원소 , = 0 + 2 + 3 + 구분 콤마 1 + 4개 원소 내부 콤마 3 = 0 + 2 + 3 + 4 
+
+// n(n+1) / 2 = comma + 1
+// n(n+1) = 2X + 2
+// n^2 + n = 2 X + 2 
+// n(n+1) = 110
+// 
+
+#include <algorithm>
+vector<int> solution(string s)
+{
+	vector<int> answer;
+	int countOfComma = std::count(s.begin(), s.end(), ',');
+	// answer.reserve();
+
+	return answer;
+}
+
 int main() 
 {
 	// solution({ {40, 2900},{23, 10000},{11, 5200},{5, 5900},{40, 3100},{27, 9200},{32, 6900} }, { 1300, 1500, 1600, 4900 });
